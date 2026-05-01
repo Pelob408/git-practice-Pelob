d985754cccab993537c39b3fa134afaa718ea17c
Pelob <pelobcse39@gmail.com>
2026-05-01 21:15:30 +0600

    Add basic calculator functions

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
-from datetime import date
+import datetime
+from utils import add, subtract
 
+print("Name: Pelob Chakraborti")
+print("Today's date:", datetime.date.today())
 
-print("Hello GitHub")
-print("Pelob")
-print(f"Today is {date.today()}")
+print("Addition:", add(5, 3))
+print("Subtraction:", subtract(5, 3))
