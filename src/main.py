c2df8a91481dc45566f2d9481007bf6e589d9672
Pelob <pelobcse39@gmail.com>
2026-05-01 23:39:34 +0600

    Add multiply function

--- src/main.py
+++ src/main.py
@@ -1,8 +1,9 @@
 import datetime
-from utils import add, subtract
+from utils import add, subtract, multiply   
 
 print("Name: Pelob Chakraborti")
 print("Today's date:", datetime.date.today())
 
 print("Addition:", add(5, 3))
 print("Subtraction:", subtract(5, 3))
+print("Multiply:", multiply(3, 4)) 
